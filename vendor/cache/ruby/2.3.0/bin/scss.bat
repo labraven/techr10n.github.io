f3586f825ade70fa3898b7df2c7b27e1ed676dc3
techr10n <ryan.hammang@live.com>
2017-01-08 06:55:53 -0700

    Syncing to Win10

---
+++ vendor/cache/ruby/2.3.0/bin/scss.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\tools\ruby23\bin\ruby.exe" "C:/Users/ryan/techr10n.github.io/vendor/cache/ruby/2.3.0/bin/scss" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\tools\ruby23\bin\ruby.exe" "%~dpn0" %*
